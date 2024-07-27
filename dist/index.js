70b6d9813a8958ceff5b34d1e663de9defb23049
robitu <robertoiturralde2009@hotmail.com>
2024-07-27 17:09:25 -0600

    one second delay

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Camera_1 = require("./components/Camera/Camera");
-exports.Camera = Camera_1.Camera;
-//# sourceMappingURL=index.js.map
